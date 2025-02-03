1bb9a7288b228237b1dce0cacc0b0cfac03fbbaf
Kaleb Rothenberger <rothenberger.kj@gmail.com>
2025-02-02 23:13:17 -0500

    Update AverageAssignment.py
    Fetching the origin did not work for me so i properly followed your instructions in the announcement that you made for us. I hope this is correct.

---
+++ AverageAssignment.py
@@ -0,0 +1,13 @@
+print("Please enter three numberes")     #Asking the User to enter 3 numbers
+
+Number1 = int(input("First number: "))   #Asking to enter the first number
+Number2 = int(input("Second number: "))  #Asking to enter the second number
+Number3 = int(input("Third number: "))   #Asking to enter the third number
+
+sum = Number1 + Number2 + Number3        #Calculating the Sum
+
+average = sum / 3                        #Calculating the Average
+
+Answer = f"The sum of {Number1}, {Number2}, and {Number3} is {sum} and the Average of all three is {average}"   # Making my statement
+
+print(Answer)                            #Printing my statement 
